cb9d0d5f3b72fd52ddd505a4cd5cadac7583346b
erik-ferreira <erikdesouzagf@gmail.com>
2022-07-27 23:16:22 -0300

    added react native firebase and complete feature login

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,8 +9,10 @@ import {
   FlatList,
   Center,
 } from "native-base";
-import { SignOut, ChatTeardropText } from "phosphor-react-native";
+import { Alert } from "react-native";
+import auth from "@react-native-firebase/auth";
 import { useNavigation } from "@react-navigation/native";
+import { SignOut, ChatTeardropText } from "phosphor-react-native";
 
 import { Filter } from "../components/Filter";
 import { Order, OrderProps } from "../components/Order";
@@ -48,6 +50,15 @@ export function Home() {
     navigation.navigate("details", { orderId });
   }
 
+  function handleLogout() {
+    auth()
+      .signOut()
+      .catch((error) => {
+        console.log(error);
+        return Alert.alert("Sair", "Não foi possível sair.");
+      });
+  }
+
   return (
     <VStack flex={1} pb={6} bg="gray.700">
       <HStack
@@ -61,7 +72,10 @@ export function Home() {
       >
         <Logo />
 
-        <IconButton icon={<SignOut size={26} color={colors.gray[300]} />} />
+        <IconButton
+          icon={<SignOut size={26} color={colors.gray[300]} />}
+          onPress={handleLogout}
+        />
       </HStack>
 
       <VStack flex={1} px={6}>
--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
-import { VStack, Heading, Icon, useTheme } from "native-base";
+import { Alert } from "react-native";
+import { VStack, Heading, Icon, useTheme, ScrollView } from "native-base";
 import { Envelope, Key } from "phosphor-react-native";
+import auth from "@react-native-firebase/auth";
 
 import { Input } from "../components/Input";
 import { Button } from "../components/Button";
@@ -12,39 +14,74 @@ function Signin() {
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
-  function handleSignIn() {
-    console.log(email, password);
+  async function handleSignIn() {
+    if (!email || !password) {
+      return Alert.alert("Entrar", "Informe email e a senha!");
+    }
+
+    try {
+      setIsLoading(true);
+
+      await auth().signInWithEmailAndPassword(email, password);
+    } catch (err) {
+      setIsLoading(false);
+
+      console.log("error", err);
+
+      if (err.code === "auth/invalid-email") {
+        return Alert.alert("Entrar", "E-mail inválido.");
+      }
+
+      if (
+        err.code === "auth/user-not-found" ||
+        err.code === "auth/wrong-password"
+      ) {
+        return Alert.alert("Entrar", "E-mail ou senha inválida.");
+      }
+
+      return Alert.alert("Entrar", "Não foi possível acessar");
+    }
   }
 
   return (
-    <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
-      <Logo />
+    <ScrollView bg="gray.600">
+      <VStack flex={1} alignItems="center" px={8} pt={24} pb={18}>
+        <Logo />
 
-      <Heading color="gray.100" mt={20} mb={6}>
-        Acesse sua conta
-      </Heading>
+        <Heading color="gray.100" mt={20} mb={6}>
+          Acesse sua conta
+        </Heading>
 
-      <Input
-        mb={4}
-        placeholder="E-mail"
-        InputLeftElement={
-          <Icon as={<Envelope color={colors.gray[300]} />} ml={4} />
-        }
-        value={email}
-        onChangeText={setEmail}
-      />
-      <Input
-        mb={8}
-        placeholder="Senha"
-        InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4} />}
-        secureTextEntry
-        value={password}
-        onChangeText={setPassword}
-      />
+        <Input
+          mb={4}
+          placeholder="E-mail"
+          InputLeftElement={
+            <Icon as={<Envelope color={colors.gray[300]} />} ml={4} />
+          }
+          value={email}
+          onChangeText={setEmail}
+        />
+        <Input
+          mb={8}
+          placeholder="Senha"
+          InputLeftElement={
+            <Icon as={<Key color={colors.gray[300]} />} ml={4} />
+          }
+          secureTextEntry
+          value={password}
+          onChangeText={setPassword}
+        />
 
-      <Button title="Entrar" w="full" onPress={handleSignIn} />
-    </VStack>
+        <Button
+          title="Entrar"
+          w="full"
+          isLoading={isLoading}
+          onPress={handleSignIn}
+        />
+      </VStack>
+    </ScrollView>
   );
 }
 
