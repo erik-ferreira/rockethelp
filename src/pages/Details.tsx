84138ea5cf2e7ea34c9dbf92eb400a6a19ba8136
erik-ferreira <erikdesouzagf@gmail.com>
2022-07-31 14:00:14 -0300

    fix in details

--- src/pages/Details.tsx
+++ src/pages/Details.tsx
@@ -140,12 +140,12 @@ export function Details() {
           title="equipamento"
           description={`Patrimônio ${order.patrimony}`}
           icon={DesktopTower}
-          footer={order.when}
         />
         <CardDetails
           title="descrição do problema"
           description={order.description}
           icon={Clipboard}
+          footer={order.when}
         />
         <CardDetails
           title="Solução"
